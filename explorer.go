c38a0f11a0c1dca4c2cae1a6a7f59d229451e835
JackXLu <jack.xujun.lu@gmail.com>
2015-03-31 13:59:06 -0500

    Added web.go

--- explorer.go
+++ explorer.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"html/template"
 	"log"
-	"net/http"
+//	"net/http"
 
 	"github.com/FactomProject/factom"
 	"github.com/FactomProject/FactomCode/database"	
+	"github.com/hoisie/web"	
 )
 
 var _ = fmt.Sprint("tmp")
 
 var (
-	handleStatic = http.FileServer(http.Dir("./"))
+	//handleStatic = http.FileServer(http.Dir("./"))
 	tpl = new(template.Template)
 	db              database.Db	
+	server = web.NewServer()	
+	
 )
 
 func init() {
@@ -25,45 +28,33 @@ func init() {
 		"views/eblock.html",
 		"views/sentry.html",
 	))
-	http.Handle("/css/", handleStatic)
-	http.Handle("/fonts/", handleStatic)
-	http.Handle("/images/", handleStatic)
-	http.Handle("/scripts/", handleStatic)
-	http.HandleFunc("/", handleDBlocks)
-	http.HandleFunc("/dblocks/", handleDBlocks)
-	http.HandleFunc("/dblock/", handleDBlock)
-	http.HandleFunc("/eblock/", handleEBlock)
-	http.HandleFunc("/sentry/", handleEntry)
-	http.HandleFunc("/search", handleSearch)	
+	server.Config.StaticDir	= "/tmp/static"
+	server.Get(`/(?:home)?`, handleHome)	
+	server.Get(`/`, handleDBlocks)
+	server.Get(`/dblocks/`, handleDBlocks)
+	server.Get(`/dblock/`, handleDBlock)
+	server.Get(`/eblock/`, handleEBlock)
+	server.Get(`/sentry/`, handleEntry)
+	server.Post(`/search/`, handleSearch)	
 }
 
 func Start(dbref database.Db) {
 	db = dbref
 	fmt.Println("explorer serving at port: 8087")	
-	http.ListenAndServe(":8087", nil)
+	//http.ListenAndServe(":8087", nil)
+	go server.Run("localhost:8087")	
 
 }
 
-func handleSearch(w http.ResponseWriter, r *http.Request) {
-	for k,v := range r.Form {
-		fmt.Println("k:", k)
-		fmt.Println("v:", v)
-	}	
-	fmt.Println("r.Form:", r.FormValue("searchText"))	
-	r.ParseForm()
-	fmt.Println("r.Form:", r.FormValue("searchText"))
-	fmt.Println("r.Form:", r.Form.Get("searchText"))
-	r.ParseMultipartForm(100000)
-	fmt.Println("r.Form:", r.FormValue("searchText"))
-	fmt.Println("r.Form:", r.Form.Get("searchText"))	
-	for k,v := range r.Form {
-		fmt.Println("k:", k)
-		fmt.Println("v:", v)
-	}
+
+func handleSearch(ctx *web.Context) {
+
+	fmt.Println("r.Form:", ctx.Params["searchText"])	
+
 	//tpl.ExecuteTemplate(w, "index.html", dBlocks)
 }
 
-func handleDBlocks(w http.ResponseWriter, r *http.Request) {
+func handleDBlocks(ctx *web.Context) {
 	height, err := factom.GetBlockHeight()
 	if err != nil {
 		log.Fatal(err)
@@ -76,38 +67,42 @@ func handleDBlocks(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("dBlocks is nil")
 	}
 
-	tpl.ExecuteTemplate(w, "index.html", dBlocks)
+	tpl.ExecuteTemplate(ctx.ResponseWriter, "index.html", dBlocks)
 }
 
-func handleDBlock(w http.ResponseWriter, r *http.Request) {
-	mr := r.URL.Path[len("/dblock/"):]
+func handleDBlock(ctx *web.Context) {
+	mr := ctx.Request.URL.Path[len("/dblock/"):]
 	
 	dblock, err := factom.GetDBlock(mr)	
 	if err != nil {
 		fmt.Println(err)
 	}
 	
-	tpl.ExecuteTemplate(w, "dblock.html", dblock)
+	tpl.ExecuteTemplate(ctx.ResponseWriter, "dblock.html", dblock)
 }
 
-func handleEBlock(w http.ResponseWriter, r *http.Request) {
-	mr := r.URL.Path[len("/eblock/"):]
+func handleEBlock(ctx *web.Context) {
+	mr := ctx.Request.URL.Path[len("/eblock/"):]
 	
 	eblock, err := factom.GetEBlock(mr)	
 	if err != nil {
 		fmt.Println(err)
 	}
 	
-	tpl.ExecuteTemplate(w, "eblock.html", eblock)
+	tpl.ExecuteTemplate(ctx.ResponseWriter, "eblock.html", eblock)
 }
 
-func handleEntry(w http.ResponseWriter, r *http.Request) {
-	hash := r.URL.Path[len("/entry/"):]
+func handleEntry(ctx *web.Context) {
+	hash := ctx.Request.URL.Path[len("/entry/"):]
 	
 	entry, err := factom.GetEntry(hash)	
 	if err != nil {
 		fmt.Println(err)
 	}
 	
-	tpl.ExecuteTemplate(w, "sentry.html", entry)
+	tpl.ExecuteTemplate(ctx.ResponseWriter, "sentry.html", entry)
+}
+
+func handleHome(ctx *web.Context) {
+	handleDBlocks(ctx)
 }
