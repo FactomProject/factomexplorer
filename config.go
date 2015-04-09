68caf79b5cfc201c2d6ee98aeed9baf18fad8e96
michaelbeam <michaeljbeam@gmail.com>
2015-04-09 14:09:19 -0500

    removed workdir config object

---
+++ config.go
@@ -0,0 +1,39 @@
+// Copyright 2015 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package factomexplorer
+
+import (
+	"os"
+	"code.google.com/p/gcfg"
+)
+
+type ExplorerConfig struct {
+	Explorer struct {
+		PortNumber	int
+		StaticDir	string
+	}
+}
+
+const defaultConfig = `
+; ------------------------------------------------------------------------------
+; Explorer Settings
+; ------------------------------------------------------------------------------
+
+[explorer]
+PortNumber	= 8087
+StaticDir	= ""
+`
+
+// ReadConfig reads the default factomexplorer.conf file and returns the
+// ExplorerConfig object corresponding to the state of the conf file.
+func ReadConfig() *ExplorerConfig {
+	cfg := new(ExplorerConfig)
+	filename := os.Getenv("HOME")+"/.factom/factomexplorer.conf"
+	err := gcfg.ReadFileInto(cfg, filename)
+	if err != nil {
+		gcfg.ReadStringInto(cfg, defaultConfig)
+	}
+	return cfg
+}
