7a02db3c72efd39d164628ec4f472a8f90ab2c65
ThePiachu <ThePiachu@gmail.com>
2015-10-19 13:21:10 -0700

    Added key sanitization and a small test.

--- app/database.go
+++ app/database.go
@@ -3,10 +3,14 @@ package app
 import (
 	"appengine"
 	"appengine/datastore"
+	"fmt"
 	"github.com/ThePiachu/Go/Datastore"
+	"unicode/utf8"
 )
 
 func LoadData(c appengine.Context, bucket, key string, dst interface{}) (interface{}, error) {
+	bucket = SanitizeKey(bucket)
+	key = SanitizeKey(key)
 	err := Datastore.GetFromDatastoreSimpleOrMemcache(c, bucket, key, bucket+key, dst)
 	if err == datastore.ErrNoSuchEntity {
 		return nil, nil
@@ -15,6 +19,15 @@ func LoadData(c appengine.Context, bucket, key string, dst interface{}) (interfa
 }
 
 func SaveData(c appengine.Context, bucket, key string, toStore interface{}) error {
+	bucket = SanitizeKey(bucket)
+	key = SanitizeKey(key)
 	_, err := Datastore.PutInDatastoreSimpleAndMemcache(c, bucket, key, bucket+key, toStore)
 	return err
+}
+
+func SanitizeKey(key string) string {
+	if utf8.Valid([]byte(key)) == false {
+		return fmt.Sprintf("%q", key)
+	}
+	return key
 }
--- app/main_test.go
+++ app/main_test.go
@@ -1,16 +1,22 @@
 package app_test
 
 import (
+	"appengine/aetest"
 	"fmt"
 	"github.com/FactomProject/factom"
+	"github.com/ThePiachu/Go/Datastore"
+	"github.com/ThePiachu/Go/mymath"
 	"testing"
+	"unicode/utf8"
 )
 
+/*
 func Test(t *testing.T) {
 	GetAddressInformationFromFactom("FA3eNd17NgaXZA3rXQVvzSvWHrpXfHWPzLQjJy2PQVQSc4ZutjC1")
 	//GetAddressInformationFromFactom("dceb1ce5778444e7777172e1f586488d2382fb1037887cd79a70b0cba4fb3dce")
 	t.Errorf("Test")
 }
+*/
 
 func GetAddressInformationFromFactom(address string) error {
 	ecBalance, err := factom.ECBalance(address)
@@ -27,4 +33,56 @@ func GetAddressInformationFromFactom(address string) error {
 	}
 
 	return nil
+}
+
+type Test struct {
+	Test string
+}
+
+func testMemcache(t *testing.T) {
+	hexStr := "ca81e518e9a5519b7b218b85b13d73447f65c48c9c6f1b67db55a54ab48fc1de"
+	hex := mymath.Str2Hex(hexStr)
+	str := mymath.Hex2ASCII(hex)
+	fmt.Printf("Test - %v\n", str)
+
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer c.Close()
+
+	toPut := new(Test)
+	toPut.Test = "test"
+
+	err = Datastore.PutInMemcache(c, str, toPut)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = Datastore.PutInDatastoreSimpleAndMemcache(c, "test", str, str, toPut)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestUFT8(t *testing.T) {
+	hexStr := "ca81e518e9a5519b7b218b85b13d73447f65c48c9c6f1b67db55a54ab48fc1de"
+	hex := mymath.Str2Hex(hexStr)
+	str := mymath.Hex2ASCII(hex)
+	fmt.Printf("Test - %v\n", str)
+
+	fmt.Println(utf8.Valid(hex))
+	str2 := SanitizeKey(str)
+	fmt.Printf("Test2 - %v\n", str2)
+	str3 := SanitizeKey("This is plaintext, nothing to see here")
+	fmt.Printf("Test2 - %v\n", str3)
+
+}
+
+func SanitizeKey(key string) string {
+	if utf8.Valid([]byte(key)) == false {
+		return fmt.Sprintf("%q", key)
+	}
+	return key
 }
